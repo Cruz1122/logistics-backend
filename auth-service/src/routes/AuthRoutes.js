bf045df515cce926510315d61431251ef475e10e
pipe1z <you@example.com>
2025-04-30 23:58:09 -0500

    Implementation of forgot-password and change-password

--- auth-service/src/routes/AuthRoutes.js
+++ auth-service/src/routes/AuthRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, verifyEmail, resendVerificationCode, login, verify2FaCode,health} = require("../controllers/AuthController");
+const { signUp, verifyEmail, resendVerificationCode, login, verify2FaCode, requestPasswordReset,resetPassword, changePassword, health} = require("../controllers/AuthController");
 
 router.post("/signup", signUp);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-verification", resendVerificationCode );
 router.post("/login", login);
 router.post("/verify2FaCode", verify2FaCode);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
+router.patch("/change-password", changePassword);
 router.get("/health", health);
 
 module.exports = router;

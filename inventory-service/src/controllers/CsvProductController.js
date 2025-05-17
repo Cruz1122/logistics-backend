2adcd32021d0a0604210660bb9a20b57c81feaaf
pipe1z <you@example.com>
2025-05-17 16:19:25 -0500

    Implementation of test CsvProductController.test.js and changes in CsvProductController in line 5: const pLimit = require(p-limit)

--- inventory-service/src/controllers/CsvProductController.js
+++ inventory-service/src/controllers/CsvProductController.js
@@ -2,10 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("csv-parser");
 const prisma = require("../config/prisma");
-let pLimit;
-(async () => {
-  pLimit = (await import("p-limit")).default;
-})();
+const pLimit = require("p-limit");
 
 const logDir = path.join(__dirname, "../../logs");
 if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);

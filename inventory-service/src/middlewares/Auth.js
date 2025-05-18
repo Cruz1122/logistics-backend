50896f1259e8b2b23860a135c5cd13a852537475
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2025-05-18 17:38:46 -0500

    feat(auth-service): enhance authorization middleware to call external auth-service for role permissions

--- inventory-service/src/middlewares/Auth.js
+++ inventory-service/src/middlewares/Auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const prisma = require("../config/prisma");
+const axios = require("axios");
 
 function authenticateJWT(req, res, next) {
   const auth = req.headers.authorization;
@@ -25,22 +26,29 @@ function authorize(permissionName, action) {
   return async (req, res, next) => {
     const { roleId } = req.user;
     try {
-      const rp = await prisma.rolePermission.findFirst({
-        where: {
-          roleId,
-          permission: { name: permissionName },
-        },
-        select: { [action]: true },
-      });
+      // Llama al auth-service para verificar el permiso
+      const response = await axios.get(
+        `${process.env.AUTH_URL}/role-permissions/role-permissions`,
+        {
+          params: {
+            roleId,
+            permissionName,
+            action,
+          },
+          headers: {
+            Authorization: req.headers.authorization,
+          },
+        }
+      );
 
-      if (!rp || rp[action] !== true) {
+      if (!response.data || response.data.allowed !== true) {
         return res
           .status(403)
           .json({ message: "No tienes permiso para realizar esta acción" });
       }
       next();
     } catch (err) {
-      console.error("Error chequeando permisos:", err);
+      console.error("Error chequeando permisos:", err.message);
       res.status(500).json({ message: "Error interno de autorización" });
     }
   };
